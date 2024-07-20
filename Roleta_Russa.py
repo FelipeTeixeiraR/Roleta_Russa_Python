f3fc6b3449d4aa3b51ac2682fac1fa967547514d
Felipe Teixeira <felipe.teixeira01.ifpr@gmail.com>
2024-07-20 01:32:16 -0300

    Começando A roleta

---
+++ Roleta_Russa.py
@@ -0,0 +1,15 @@
+import random
+
+life = int
+int = bullet = random.randrange(3, 10)
+int = vazia = random.randrange(1, bullet)
+int = ativa = bullet - vazia
+c = 'c'
+d = 'd'
+int = carga = [c] * ativa + [d] * vazia
+random.shuffle(carga)
+print(carga)
+
+print(bullet)
+print(vazia)
+print(ativa)
